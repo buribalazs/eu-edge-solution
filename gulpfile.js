02071aba8a533ffeae17b50dc9d2efecd524075d
Buri Balázs <buribalazs@gmail.com>
2017-01-26 11:53:59 +0100

    css and html minify for --dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     livereload = require('gulp-livereload'),
+    htmlmin = require('gulp-htmlmin'),
     minify = require('gulp-minify'),
     sass = require('gulp-sass');
 
 console.log(gutil.env);
 
 gulp.task('html', function () {
+    var process = gutil.env.dist ? htmlmin : gutil.noop;
     gulp.src('src/**/*.html')
+        .pipe(process({collapseWhitespace: true}))
         .pipe(gulp.dest('dist/'))
         .pipe(livereload());
 });
@@ -20,7 +23,7 @@ gulp.task('asset', function () {
 
 gulp.task('sass', function () {
     gulp.src('src/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: gutil.env.dist ? 'compressed' : undefined}).on('error', sass.logError))
         .pipe(gulp.dest('dist/css'))
         .pipe(livereload());
 });
@@ -28,7 +31,7 @@ gulp.task('sass', function () {
 gulp.task('js-project', function () {
     var process = gutil.env.dist ? minify : gutil.noop;
     gulp.src('src/js/**/*.js')
-        .pipe(process({noSource:true, ext:{min:'.js'}}))
+        .pipe(process({noSource: true, ext: {min: '.js'}}))
         .pipe(gulp.dest('dist/js/'))
         .pipe(livereload());
 });
